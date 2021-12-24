471cf9ccf83f3349151329f920f2ef2635af748c
mohamad <devfarsi@yahoo.com>
2021-12-24 15:24:02 -0500

    optimize some classes

--- lib/pages/MainPageView.dart
+++ lib/pages/MainPageView.dart
@@ -1,90 +1,26 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rj_stories/providers/StoryProvider.dart';
 import 'package:rj_stories/responses/StoryModel.dart';
 import 'package:video_player/video_player.dart';
 
-void main() {
-  HttpOverrides.global = new MyHttpOverrides();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MainPageView extends StatefulWidget {
+  const MainPageView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Radio Javan Story',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: const MyHomePage(title: 'Radio Javan Story'),
-    );
-  }
+  _MainPageViewState createState() => _MainPageViewState();
 }
 
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainPageViewState extends State<MainPageView> {
   StroyProvider _provider = StroyProvider();
   List<StoryModel> _list = [];
   late VideoPlayerController _controller;
   late PageController _pageController;
   int _currentIndex = 0;
   String title = '';
-
   @override
-  void initState() {
-    super.initState();
-    _pageController = PageController();
-    _provider.addListener(() {
-      setState(() {
-        _list = _provider.items;
-        if (_list.length > _currentIndex) {
-          _controller =
-              VideoPlayerController.network(_list[_currentIndex].hqLink!)
-                ..initialize().then((_) {
-                  setState(() {});
-                  _controller.play();
-                });
-        }
-        _controller.addListener(() {
-          if (_controller.value.position ==
-              _controller
-                  .value.duration) if (_currentIndex + 1 < _list.length) {
-            _changePlayerState(_currentIndex + 1);
-          }
-        });
-      });
-    });
-    _provider.fetchData();
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Widget build(BuildContext context) {
+    return MainPageView();
   }
 
   void _changePlayerState(int index) {
@@ -103,45 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
-  }
-
-  GestureTapDownCallback onTapDown(double width) {
-    return (details) {
-      if (_controller.value.isBuffering) return;
-      if (details.globalPosition.dx < width / 3) {
-        if (_currentIndex - 1 > 0) {
-          _changePlayerState(_currentIndex - 1);
-        }
-        return;
-      }
-      if (details.globalPosition.dx > 2 * width / 3) {
-        if (_currentIndex + 1 < _list.length) {
-          _changePlayerState(_currentIndex + 1);
-        }
-        return;
-      }
-    };
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-
-    return Scaffold(
-      body: GestureDetector(
-        onTapDown: onTapDown(width),
-        child: Center(
-            child:
-                _list.isEmpty ? CupertinoActivityIndicator() : MainPageView()),
-      ),
-    );
-  }
-
-  PageView MainPageView() {
+  PageView MyPageView() {
     return PageView.builder(
       physics: NeverScrollableScrollPhysics(),
       onPageChanged: (index) => _changePlayerState(index),
